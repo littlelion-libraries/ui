5ed110db5fba2d6fe5cd03f03856fcc6dc6f65e4
DESKTOP-UJRR6FS\Admin <tuduchoang@gmail.com>
2023-04-09 12:17:42 +0700

    1.0.0

---
+++ IScrollRect.cs
@@ -0,0 +1,13 @@
+﻿using Transforms;
+
+namespace UI
+{
+    public interface IScrollRect
+    {
+        IRectTransform Content { get; }
+        bool Horizontal { set; }
+        float HorizontalNormalizedPosition { set; }
+        bool Vertical { set; }
+        float VerticalNormalizedPosition { set; }
+    }
+}
---
+++ UnityScrollRect.cs
@@ -0,0 +1,35 @@
+﻿using Transforms;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class UnityScrollRect : ScrollRect, IScrollRect
+    {
+        private IRectTransform _content;
+
+        public IRectTransform Content => _content ??= new UnityRectTransform
+        {
+            Impl = content
+        };
+
+        public bool Horizontal
+        {
+            set => horizontal = value;
+        }
+
+        public float HorizontalNormalizedPosition
+        {
+            set => horizontalNormalizedPosition = value;
+        }
+
+        public bool Vertical
+        {
+            set => vertical = value;
+        }
+
+        public float VerticalNormalizedPosition
+        {
+            set => verticalNormalizedPosition = value;
+        }
+    }
+}
