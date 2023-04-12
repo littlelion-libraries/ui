a8bf267d8999f62c66e4b779536dcf1e693f5bf2
DESKTOP-UJRR6FS\Admin <tuduchoang@gmail.com>
2023-04-12 21:41:30 +0700

    1.2.1

--- UnityGridLayout.cs
+++ UnityGridLayout.cs
@@ -1,4 +1,5 @@
-﻿using Transforms;
+﻿using System;
+using Transforms;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -14,6 +15,40 @@ namespace UI
             Impl = GetComponent<RectTransform>()
         };
 
-        public float Width => preferredWidth;
+        public float Width => constraint switch
+        {
+            Constraint.Flexible => padding.left + padding.right + Size(
+                RoundUpDivision(transform.childCount, Count(
+                    cellSize.y,
+                    spacing.y,
+                    Transform.Height - padding.top - padding.bottom
+                )),
+                cellSize.x,
+                spacing.x
+            ),
+            _ => throw new NotImplementedException()
+        };
+
+        private static int Count(float cs, float s, float size)
+        {
+            var count = 0;
+            while (cs <= size)
+            {
+                count++;
+                size -= cs + s;
+            }
+
+            return count;
+        }
+
+        private int RoundUpDivision(int a, int b)
+        {
+            return (a + b - 1) / b;
+        }
+
+        private static float Size(int count, float cs, float s)
+        {
+            return count * cs + (count - 1) * s;
+        }
     }
 }
