07f9a4f9300da9cd9bfb1f7673bff5f4c25e63c7
DESKTOP-UJRR6FS\Admin <tuduchoang@gmail.com>
2023-04-12 20:50:28 +0700

    1.1.0

---
+++ ILayout.cs
@@ -0,0 +1,8 @@
+﻿namespace UI
+{
+    public interface ILayout
+    {
+        float Height { get; }
+        float Width { get; }
+    }
+}
---
+++ UnityGridLayout.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine.UI;
+
+namespace UI
+{
+    public class UnityGridLayout : GridLayoutGroup, ILayout
+    {
+        public float Height { get => preferredHeight; }
+        public float Width { get => preferredWidth; }
+    }
+}
